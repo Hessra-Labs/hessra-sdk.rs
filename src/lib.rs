b5c2858d2fd7fbbaf57b039fd0473485fd930f6a
Jacob Valentic <jake@hessra.net>
2025-02-24 11:23:26 -0800

    Adds HTTP3

--- src/lib.rs
+++ src/lib.rs
@@ -24,8 +24,18 @@ pub struct VerifyTokenResponse {
     pub response_msg: String,
 }
 
+#[cfg(feature = "http3")]
+use {
+    bytes::Bytes,
+    futures::future,
+    h3_quinn::quinn::{self, Endpoint},
+    quinn_proto::crypto::rustls::QuicClientConfig,
+    std::net::SocketAddr,
+    std::sync::Arc,
+};
+
 #[derive(Clone)]
-struct Config {
+pub struct BaseConfig {
     base_url: String,
     port: Option<u16>,
     mtls_key: String,
@@ -33,25 +43,55 @@ struct Config {
     server_ca: String,
 }
 
-pub struct HessraClient {
-    config: Config,
+impl BaseConfig {
+    fn get_base_url(&self) -> String {
+        match self.port {
+            Some(port) => format!("{}:{}", self.base_url, port),
+            None => self.base_url.clone(),
+        }
+    }
+}
+
+pub struct Http1Client {
+    config: BaseConfig,
     client: Client,
 }
 
+#[cfg(feature = "http3")]
+pub struct Http3Client {
+    config: BaseConfig,
+    endpoint: Endpoint,
+}
+
+pub enum HessraClient {
+    Http1(Http1Client),
+    #[cfg(feature = "http3")]
+    Http3(Http3Client),
+}
+
 pub struct HessraClientBuilder {
-    config: Config,
+    config: BaseConfig,
+    protocol: Protocol,
+}
+
+#[derive(Clone)]
+pub enum Protocol {
+    Http1,
+    #[cfg(feature = "http3")]
+    Http3,
 }
 
 impl HessraClientBuilder {
     pub fn new() -> Self {
         HessraClientBuilder {
-            config: Config {
+            config: BaseConfig {
                 base_url: "".to_string(),
                 port: None,
                 mtls_key: "".to_string(),
                 mtls_cert: "".to_string(),
                 server_ca: "".to_string(),
             },
+            protocol: Protocol::Http1,
         }
     }
 
@@ -80,7 +120,12 @@ impl HessraClientBuilder {
         self
     }
 
-    pub fn build(self) -> Result<HessraClient, Box<dyn Error>> {
+    pub fn protocol(mut self, protocol: Protocol) -> Self {
+        self.protocol = protocol;
+        self
+    }
+
+    fn build_http1(&self) -> Result<Http1Client, Box<dyn Error>> {
         let mut identity_pem = Vec::new();
         identity_pem.extend(self.config.mtls_cert.as_bytes());
         identity_pem.extend(self.config.mtls_key.as_bytes());
@@ -93,10 +138,54 @@ impl HessraClientBuilder {
             .use_rustls_tls()
             .build()?;
 
-        Ok(HessraClient {
-            config: self.config,
+        Ok(Http1Client {
+            config: self.config.clone(),
             client,
         })
+    }
+
+    #[cfg(feature = "http3")]
+    fn build_http3(&self) -> Result<Http3Client, Box<dyn Error>> {
+        let mut identity_pem = Vec::new();
+        identity_pem.extend(self.config.mtls_cert.as_bytes());
+        identity_pem.extend(self.config.mtls_key.as_bytes());
+
+        let roots = reqwest::Certificate::from_pem(self.config.server_ca.as_bytes())?;
+        let cert_chain = reqwest::Identity::from_pem(&identity_pem)?;
+
+        rustls::crypto::ring::default_provider()
+            .install_default()
+            .expect("Failed to install rustls crypto provider");
+
+        let mut client_crypto = rustls::ClientConfig::builder()
+            .with_root_certificates(roots.to_owned())
+            .with_client_auth_cert(cert_chain.to_owned(), key)
+            .map_err(|e| format!("Failed to build client crypto: {}", e))?;
+
+        client_crypto.alpn_protocols = vec![b"h3".to_vec()];
+        client_crypto.enable_early_data = true;
+
+        let client_config = quinn::ClientConfig::new(Arc::new(
+            QuicClientConfig::try_from(client_crypto)
+                .map_err(|e| format!("Failed to create QUIC config: {}", e))?,
+        ));
+
+        let bind_addr: SocketAddr = "0.0.0.0:0".parse()?;
+        let mut endpoint = quinn::Endpoint::client(bind_addr)?;
+        endpoint.set_default_client_config(client_config);
+
+        Ok(Http3Client {
+            config: self.config.clone(),
+            endpoint,
+        })
+    }
+
+    pub fn build(self) -> Result<HessraClient, Box<dyn Error>> {
+        match self.protocol {
+            Protocol::Http1 => Ok(HessraClient::Http1(self.build_http1()?)),
+            #[cfg(feature = "http3")]
+            Protocol::Http3 => Ok(HessraClient::Http3(self.build_http3()?)),
+        }
     }
 }
 
@@ -106,29 +195,100 @@ impl Default for HessraClientBuilder {
     }
 }
 
+#[cfg(feature = "http3")]
+impl Http3Client {
+    async fn send_request<T, R>(
+        &self,
+        endpoint: &str,
+        request_body: &T,
+    ) -> Result<R, Box<dyn Error>>
+    where
+        T: serde::Serialize,
+        R: serde::de::DeserializeOwned,
+    {
+        let server_addr: SocketAddr = format!(
+            "{}:{}",
+            self.config.base_url,
+            self.config.port.unwrap_or(443)
+        )
+        .parse()
+        .map_err(|e| format!("Invalid server address: {}", e))?;
+
+        let connection = self
+            .endpoint
+            .connect(server_addr, &self.config.base_url)?
+            .await
+            .map_err(|e| format!("Failed to connect: {}", e))?;
+
+        let quinn_conn = h3_quinn::Connection::new(connection);
+        let (mut driver, mut send_request) = h3::client::new(quinn_conn).await?;
+
+        let request = http::Request::builder()
+            .uri(format!(
+                "https://{}/{}",
+                self.config.get_base_url(),
+                endpoint
+            ))
+            .header("content-type", "application/json")
+            .body(())?;
+
+        let mut stream = send_request.send_request(request).await?;
+        let body = serde_json::to_string(request_body)?;
+        stream.send_data(Bytes::from(body)).await?;
+        stream.finish().await?;
+
+        let response = stream.recv_response().await?;
+        let mut response_data = Vec::new();
+
+        while let Some(chunk) = stream.recv_data().await? {
+            response_data.extend_from_slice(chunk.chunk());
+        }
+
+        let response: R = serde_json::from_slice(&response_data)?;
+        Ok(response)
+    }
+}
+
+impl Http1Client {
+    async fn send_request<T, R>(
+        &self,
+        endpoint: &str,
+        request_body: &T,
+    ) -> Result<R, Box<dyn Error>>
+    where
+        T: serde::Serialize,
+        R: serde::de::DeserializeOwned,
+    {
+        let response = self
+            .client
+            .post(format!("{}/{}", self.config.get_base_url(), endpoint))
+            .json(request_body)
+            .send()
+            .await?
+            .json::<R>()
+            .await?;
+
+        Ok(response)
+    }
+}
+
 impl HessraClient {
     pub fn builder() -> HessraClientBuilder {
         HessraClientBuilder::new()
     }
 
-    fn get_base_url(&self) -> String {
-        match self.config.port {
-            Some(port) => format!("{}:{}", self.config.base_url, port),
-            None => self.config.base_url.clone(),
-        }
-    }
-
     pub async fn request_token(&self, resource: String) -> Result<String, Box<dyn Error>> {
         let token_request = TokenRequest { resource };
 
-        let response = self
-            .client
-            .post(format!("{}/request_token", self.get_base_url()))
-            .json(&token_request)
-            .send()
-            .await?
-            .json::<TokenResponse>()
-            .await?;
+        let response: TokenResponse = match self {
+            HessraClient::Http1(client) => {
+                client.send_request("request_token", &token_request).await?
+            }
+            #[cfg(feature = "http3")]
+            HessraClient::Http3(client) => {
+                client.send_request("request_token", &token_request).await?
+            }
+        };
 
         response.token.ok_or_else(|| "No token in response".into())
     }
@@ -140,14 +300,19 @@ impl HessraClient {
     ) -> Result<String, Box<dyn Error>> {
         let verify_token_request = VerifyTokenRequest { token, resource };
 
-        let response = self
-            .client
-            .post(format!("{}/verify_token", self.get_base_url()))
-            .json(&verify_token_request)
-            .send()
-            .await?
-            .json::<VerifyTokenResponse>()
-            .await?;
+        let response: VerifyTokenResponse = match self {
+            HessraClient::Http1(client) => {
+                client
+                    .send_request("verify_token", &verify_token_request)
+                    .await?
+            }
+            #[cfg(feature = "http3")]
+            HessraClient::Http3(client) => {
+                client
+                    .send_request("verify_token", &verify_token_request)
+                    .await?
+            }
+        };
 
         Ok(response.response_msg)
     }
